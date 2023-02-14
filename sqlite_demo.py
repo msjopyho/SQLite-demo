c125bc7b9924d7e925332761278a38d74445f6ba
msjopyho <117312312+msjopyho@users.noreply.github.com>
2023-02-14 16:14:28 -0600

    using dictionary and tuple for input

--- sqlite_demo.py
+++ sqlite_demo.py
@@ -17,20 +17,25 @@ c = conn.cursor()
 emp_1 = Employee('John', 'Doe', 80000)
 emp_2 = Employee('Jane', 'Doe', 60000)
 
-print(emp_1.first)
-print(emp_1.last)
-print(emp_1.pay)
+#using DB API placeholder to input
+#using tuple for all the values
+# c.execute("INSERT INTO employees VALUES (?, ?, ?)", (emp_1.first, emp_1.last, emp_1.pay))
+#
+# conn.commit()
+#
+# #dictionary method
+# c.execute("INSERT INTO employees VALUES (:first, :last, :pay)", {'first': emp_2.first, 'last': emp_2.last, 'pay': emp_2.pay})
+#
+# conn.commit()
 
-
-#c.execute("INSERT INTO employees VALUES ('Mary', 'Schafer', 70000)")
-
-#conn.commit()
-
-c.execute("select * from employees where last = 'Schafer'")
+c.execute("select * from employees where last = ?", ('Schafer',))
 
 #print(c.fetchone()) #get next row in results and only return that row
 print(c.fetchall()) #get other rows as list
 
+c.execute("select * from employees where last =:last", {'last': 'Doe'})
+
+print(c.fetchall()) #get other rows as list
 
 conn.commit() #commit first transaction
 
